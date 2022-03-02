3f1dfe9a2c7d9a7610dbd9b8a513f7297a613b72
Shir <shir.avraham13@Gmail.com>
2022-03-02 11:17:54 +0200

    formatted "date" & "paid by" data

--- client/src/components/Records.js
+++ client/src/components/Records.js
@@ -1,9 +1,18 @@
 import { Table } from "antd";
 import React from "react";
 import _ from "lodash";
+import { DateTime } from "luxon";
 
 const Records = (props) => {
   const { records } = props;
+
+  const recordsToRender = records.map((record) => {
+    return {
+      ...record,
+      paid: record.shir_avraham > record.shaked_hadas ? "Shir" : "Shaked",
+      date: DateTime.fromISO(record.date).toFormat("dd/MM/yyyy"),
+    };
+  });
 
   const categories = _.groupBy(records, (record) => record.category);
   const columns = [
@@ -48,7 +57,7 @@ const Records = (props) => {
 
   return (
     <Table
-      dataSource={records}
+      dataSource={recordsToRender}
       columns={columns}
       pagination={{ defaultPageSize: 20 }}
     />
